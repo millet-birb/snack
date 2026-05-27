df347d5c6f3c65ae45d210a8bc729e479206410a
woodzzung <yoojunghan0103@gmail.com>
2026-05-27 18:23:10 +0900

    supabase

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,11 @@ app = FastAPI(
 )
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 개발 단계에서만
+     CORSMiddleware,
+    allow_origins=["https://<snack-woad.vercel.app>"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type"],
 )
 
 app.include_router(products_router, prefix="/api")
